9ba1bc31dfc544dc814c6e01ac2e29332697932c
aagwali <aagwali@vente-privee.com>
2020-10-14 21:00:31 +0200

    log option is top second set state parameter

--- src/domain/pcm/index.ts
+++ src/domain/pcm/index.ts
@@ -1,19 +1,20 @@
 import { is } from "rambda"
 import { defaultValue } from "../../_mocks"
-import { defaultWhen, equalError, setState, wrapWhen } from "../core"
+import { defaultWhen, isAppError, setState, wrapWhen } from "../core"
 import { stateError } from "../core/behaviors"
 import { getProductViewById } from "./services"
-import { PcmErrors, PcmProductView_, productView } from "./types"
+import { PcmErrors, PcmProductView, PcmProductView_, productView } from "./types"
 
-export const getProductView = (randomResult: number): productView<any> =>
-  setState({ productView: PcmProductView_ }, async () => {
+export const getProductView = (randomResult: number): productView<PcmProductView> =>
+  setState({ productView: PcmProductView_ })(async () => {
     const result = await getProductViewById(randomResult)
-      .catch(defaultWhen([equalError(PcmErrors.Conflict), defaultValue]))
+      .catch(defaultWhen([isAppError(PcmErrors.Conflict), defaultValue]))
       .catch(wrapWhen([PcmErrors.NotFound], randomResult))
 
     if (!is(Object, result) || !("response" in result))
       throw stateError(PcmErrors.InvalidResponse, "response")
 
-    const productView = result.response.data
+    const productView = result?.response?.data
+
     return { productView }
   })
--- src/domain/utils/index.ts
+++ src/domain/utils/index.ts
@@ -5,17 +5,19 @@ import { LogTypes } from "../core/types"
 import { generateRandom } from "./behaviors"
 import { randomResult, UtilsErrors } from "./types"
 
-// 0 : works fine
-// 1 : guard
-// 2 : NotFound > Wrapped
-// 3 : Conflict > Default
+/* Legend default :
+   0 : works fine
+   1 : guard
+   2 : NotFound > Wrapped
+   3 : Conflict > Default */
 
 export const getRandomNumber = (floor: string): randomResult<number> =>
-  setState({ randomResult: number }, async () => {
-    const randomResult = defaultWhen([equals(0), 3])(
-      generateRandom(Number(floor))
-    )
+  setState(
+    { randomResult: number },
+    { log: LogTypes.Fluent }
+  )(async () => {
+    const randomResult = defaultWhen([equals(0), 3])(generateRandom(Number(floor)))
     await raiseWhen([randomResult, equals(1)])(UtilsErrors.BadRandomResult)
 
     return { randomResult }
-  }, {log: LogTypes.Fluent})
+  })
